f1eeadca65e7a98c9304e2ae0fe5f1e4b306e6a5
暁东邪 <xiaodongxiexie@users.noreply.github.com>
2017-05-16 22:04:31 +0800

    Update hurst.py

--- hurst.py
+++ hurst.py
@@ -45,3 +45,47 @@ def calcHurst2(ts):
         RS = np.mean(L)
         RSlist.append(RS)
     return np.polyfit(np.log(range(2+len(RSlist),2,-1)), np.log(RSlist), 1)[0]
+
+
+def hurst(history):
+    '''只是将时间序列分为1、2、4、8、16、32等份'''
+    daily_return = list(100*history.pct_change())[1:]
+    # print(daily_return)
+    ranges = ['1','2','4','8','16','32']
+    lag = Series(index = ranges)
+    for i in range(len(ranges)):
+        if i==0:
+            lag[i] = len(daily_return)
+        else:
+            lag[i] = lag[0]//(2**i)
+
+    ARS = Series(index = ranges)
+    
+    for r in ranges:
+        #RS用来存储每一种分割方式中各个片段的R/S值
+        RS = list()
+        #第i个片段 
+        for i in range(int(r)):
+            #用Range存储每一个片段数据
+            Range = daily_return[int(i*lag[r]):int((i+1)*lag[r])]
+            sigma = np.std(Range)
+            mean = np.mean(Range)
+            for k in range(1,len(Range)-1):
+                Range[k+1]=Range[k]+Range[k+1]
+            
+            Deviation = Range - mean
+            maxi = max(Deviation)
+            mini = min(Deviation)
+            RS.append(maxi - mini)
+            # sigma = np.std(Range)
+            RS[i] = RS[i]/sigma
+            # print(maxi-mini)
+            # print(Range)
+        ARS[r] = np.mean(RS)
+        # print(RS)
+    lag = np.log10(lag)
+    ARS = np.log10(ARS)
+    hurst_exponent = np.polyfit(lag,ARS,1)
+    # hurst_exponent = (sm.OLS(ARS.values,lag.values)).fit()
+    hurst = hurst_exponent[0]
+    return hurst
